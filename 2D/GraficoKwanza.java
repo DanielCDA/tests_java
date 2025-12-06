41ddb83865323d3b28a3ba5e74b6c80e17383f34
Daniel CDA <danielagostinho15@gmail.com>
2025-12-06 23:13:15 +0100

    grafico da moeda

--- 2D/GraficoKwanza.java
+++ 2D/GraficoKwanza.java
@@ -1,8 +1,10 @@
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import javax.swing.*;
 
 public class GraficoKwanza extends JPanel {
-    int[] valores = {5000, 12000, 8000, 15000}; // valores fictícios em Kz
+    
+    int[] valores = {5000, 12000, 8000, 15000}; // valores em Kz
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -11,13 +13,20 @@ public class GraficoKwanza extends JPanel {
 
         int x = 50;
         for (int valor : valores) {
+            AffineTransform at = new AffineTransform();
+            at.translate(x, 200); // mover barra para posição
+            at.scale(1, valor / 5000.0); // escalar altura proporcional ao valor
+
+            g2.setTransform(at);
             g2.setColor(Color.BLUE);
-            g2.fillRect(x, 200 - (valor / 100), 40, valor / 100);
+            g2.fillRect(0, -40, 40, 40); // barra escalada
             g2.setColor(Color.BLACK);
-            g2.drawString(valor + " Kz", x, 220);
+            g2.drawString(valor + " Kz", 0, 20);
+
             x += 60;
         }
     }
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Gráfico em Kwanza");
