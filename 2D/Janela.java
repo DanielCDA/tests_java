8b25681fd6d8b0e43eb6270dd1e2cf490e8c6a4a
Daniel CDA <danielagostinho15@gmail.com>
2025-12-06 21:41:05 +0100

    Criando minha primeira janela

---
+++ 2D/Janela.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+
+
+public class Janela {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame ("Janela do Daniel!");
+        janela.setSize(400, 300);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+    }
+    
+}
