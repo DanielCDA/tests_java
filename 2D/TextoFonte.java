0a2aab92094836b13e5d80e12dc4188e5fbfd5c8
Daniel CDA <danielagostinho15@gmail.com>
2025-12-08 11:00:53 +0100

    acrescentando o render para suavisar as letras

--- 2D/TextoFonte.java
+++ 2D/TextoFonte.java
@@ -1,6 +1,4 @@
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,6 +8,11 @@ public class TextoFonte extends JPanel {
     {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
         g2.setFont(new Font("Serif", Font.BOLD, 15));
         g2.drawString("Daniel es lindo",100,80);
